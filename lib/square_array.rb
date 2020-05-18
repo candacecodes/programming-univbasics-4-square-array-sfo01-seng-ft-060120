b6bb37e6676b5536d1457f66e36e15257dc60c7b
eager-class-5814 <candacehchoe@gmail.com>
2020-05-18 21:13:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
 new_numbers = []
 counter = 0 
  while counter < array.length do
-   new_numbers = array[counter] * array[counter]
+  puts new_numbers = array[counter] * array[counter]
    counter += 1
  end
  new_numbers
